7360f4013c270ccf8c7550ac17023e9cf89b652f
mihaaly <mihaaly@gmail.com>
2017-04-05 16:24:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/populationquiz/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.populationquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Starts quiz.
+     */
+
+    public void startQuiz (View view) {
+        Intent intent = new Intent(this,QuizActivity.class);
+        startActivity(intent);
+
+    }
+}
+
+
